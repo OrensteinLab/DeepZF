0670e7206d3b36f43f033072f1249a8b3fc5d463
sofiaaizen <66733382+sofiaaizen@users.noreply.github.com>
2022-04-16 19:50:41 +0300

    Update eval_PWMpredictor.py

--- PWMpredictor/result_analysis/eval_PWMpredictor.py
+++ PWMpredictor/result_analysis/eval_PWMpredictor.py
@@ -11,7 +11,6 @@ def user_input():
     parser.add_argument('-p_add', '--pred_folder_add', help='c_rc predictions folder add', type=str, required=True)
     parser.add_argument('-c_rc_add', '--c_rc_add', help='c_rc data frame folder add', type=str, required=True)
     parser.add_argument('-s_add', '--s_add', help='saving folder add', type=str, required=True)
-    parser.add_argument('-exp_name', '--exp_name', help='type of experiment', type=str, required=True)
 
     args = parser.parse_args()
     arguments = vars(args)
@@ -39,7 +38,7 @@ def main(args):
         pred_mat = np.concatenate((pred_mat, pred_list[i+1]), axis=0)
 
     gt_mat = (c_rc_df.filter(items=['A1', 'C1', 'G1', 'T1', 'A2', 'C2', 'G2', 'T2', 'A3', 'C3', 'G3', 'T3'])).values # ground truth mat
-    cal_p_c_per_pos(pred_mat, gt_mat, args["s_add"], args["exp_name"])  # calculate mean pearson correlation for each position
+    cal_p_c_per_pos(pred_mat, gt_mat, args["s_add"])  # calculate mean pearson correlation for each position
 
 
 if __name__ == "__main__":
