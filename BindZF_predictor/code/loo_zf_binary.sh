af98e958f38d249cf0520581b0ae1fc5b7dc7c7e
sofiaaizen <66733382+sofiaaizen@users.noreply.github.com>
2022-04-16 12:27:17 +0300

    Delete loo_zf_binary.sh

--- BindZF_predictor/code/loo_zf_binary.sh
+++
@@ -1,9 +0,0 @@
-
-data_name="${i}_zf_${i}_b"
-f="loo_bc/with_dup/gpu/${data_name}"
-mkdir -p $f
-mkdir -p ${f}/predictions
-python3.6 main_bindzfpredictor.py -b_n ${data_name} -r 1 -p_add ${f} >> out
-
-
-
